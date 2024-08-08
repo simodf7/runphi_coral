75024ead45e212d8b6a31419fe6d7cd439cce7e6
Luigi De Simone <luigi.desimone@unina.it>
2024-08-08 18:22:31 +0200

    fix ping_pong_zephyr.sh script in qemu-jailhouse envirnment

--- environment/qemu/jailhouse/install/root/scripts_jailhouse_qemu/demos/zephyr_ping_pong_demo/ping_pong_zephyr.sh
+++ environment/qemu/jailhouse/install/root/scripts_jailhouse_qemu/demos/zephyr_ping_pong_demo/ping_pong_zephyr.sh
@@ -1,9 +1,10 @@
-
 #!/bin/bash
 
 #WARNING: configure with the right .cell
-ZEPHYR_INMATE_CELL="qemu-arm64-zephyr-demo1.cell"
+ZEPHYR_INMATE_NAME="qemu-arm64-zephyr-non-rootcell1"
+ZEPHYR_INMATE_CELL="qemu-arm64-zephyr-non-rootcell1.cell"
+ARCH="arm64"
 
 jailhouse cell create ${JAILHOUSE_DIR}/configs/arm64/${ZEPHYR_INMATE_CELL}
-jailhouse cell load qemu-arm64-zephyr-demo1 ${NON_ROOTCELL_DIR}/microros_zephyr_ping_pong/zephyr.bin -a 0x70000000
-jailhouse cell start qemu-arm64-zephyr-demo1
+jailhouse cell load ${ZEPHYR_INMATE_NAME} ${NON_ROOTCELL_DIR}/${ARCH}/zephyr_ping_pong.bin -a 0x70000000
+jailhouse cell start ${ZEPHYR_INMATE_NAME}
