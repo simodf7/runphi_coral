2b5564f541f866757211d46bb2f7bc339f1d48cb
Daniele Ottaviano <dottavia@theia.tum.de>
2024-10-10 14:36:38 +0200

    Updated Kria setup: scripts, environment config, linux_defconfig, devicetree, u-boot, u-boot_defconfig
    Signed-off-by: Daniele Ottaviano <dottavia@theia.tum.de>

--- environment/zcu104/jailhouse/install/etc/profile.d/jailhouse_path.sh
+++ environment/zcu104/jailhouse/install/etc/profile.d/jailhouse_path.sh
@@ -1,3 +1,4 @@
 export PATH="/root/jailhouse/tools/:$PATH"
 export JAILHOUSE_DIR="/root/jailhouse"
-export NON_ROOTCELL_DIR="/root/non_rootcell"
+export NON_ROOTCELL_DIR="${JAILHOUSE_DIR}/inmates/demos"
+export NON_ROOTCELL_LINUX_DIR="/root/non_rootcell/linux"
