573b921b17aae5066cba5e76bfdd5517f28e472e
Francesco Boccola <f.boccola@studenti.unina.it>
2024-11-07 15:39:23 +0100

    Update cluster_setup.sh
    Now copies the appropriate .toml files and cell.h

--- scripts/orchestration/cluster_setup.sh
+++ scripts/orchestration/cluster_setup.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
 
-current_dir=$(dirname -- "$(readlink -f -- "$0")")
+usage() {
+  echo -e "Usage: $0 \r\n \
+  This script copy the pub key to the remote target:\r\n \
+    [-t <target>]\r\n \
+    [-b <backend>]\r\n \
+    [-h help]" 1>&2
+  exit 1
+}
+
+current_dir=$(dirname -- "$(readlink -f -- "$1")")
 script_dir=$(dirname -- "$(readlink -f -- "$current_dir")")
 env_builder_dir=$(dirname -- "$(readlink -f -- "$script_dir")")
+source ${script_dir}/common/common.sh
+
+
+while getopts "t:b:h" o; do
+  case "${o}" in
+  t)
+    TARGET=${OPTARG}
+    ;;
+  b)
+    BACKEND=${OPTARG}
+    ;;
+  h)
+    usage
+    ;;
+  *)
+    usage
+    ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+# Set the Environment
+source ${script_dir}/common/set_environment.sh ${TARGET} ${BACKEND}
 
 RUNPHI_DIR=$1
 #CONTROL_PLANE_IP="192.168.100.21"
@@ -68,7 +100,7 @@ echo "executing ${join_cmd} on ${RUNPHI_NODE_IP}"
 
 ## Reset for sake of simplicity and avoid inconsistency
 ssh ${RUNPHI_NODE_USER}@${RUNPHI_NODE_IP} "kubeadm reset -f; ps -ef | grep /usr/bin/kubelet | awk '{print \$1}' | xargs kill -9 "
-ssh ${CONTROL_PLANE_USER}@${CONTROL_PLANE_IP} "export export KUBECONFIG=/etc/kubernetes/admin.conf; kubectl delete node ${RUNPHI_NODE_HOSTNAME}"
+ssh ${CONTROL_PLANE_USER}@${CONTROL_PLANE_IP} "export KUBECONFIG=/etc/kubernetes/admin.conf; kubectl delete node ${RUNPHI_NODE_HOSTNAME}"
 
 ## Copy again the configuration file dumped out by the kube reset
 scp ${env_builder_dir}/environment/qemu/jailhouse/install/var/lib/kubelet/configBR.yaml ${RUNPHI_NODE_USER}@${RUNPHI_NODE_IP}:/var/lib/kubelet/configBR.yaml
@@ -77,7 +109,7 @@ scp ${env_builder_dir}/environment/qemu/jailhouse/install/var/lib/kubelet/config
 ssh ${RUNPHI_NODE_USER}@${RUNPHI_NODE_IP} "/root/target/network_config_cluster.sh"
 
 ## Make the node join the cluster
-ssh ${RUNPHI_NODE_USER}@${RUNPHI_NODE_IP} "export KUBECONFIG=/etc/kubernetes/admin.conf;${join_cmd}" &
+ssh ${RUNPHI_NODE_USER}@${RUNPHI_NODE_IP} "modprobe br_netfilter; export KUBECONFIG=/etc/kubernetes/admin.conf;${join_cmd}" &
 PID_JOIN=$!
 echo "Waiting for the node to join"
 sleep 20
@@ -86,5 +118,26 @@ sleep 20
 ssh ${RUNPHI_NODE_USER}@${RUNPHI_NODE_IP} "/etc/init.d/S92kubelet start"
 wait $PID_JOIN
 ssh ${RUNPHI_NODE_USER}@${RUNPHI_NODE_IP} "mkdir -p /usr/share/runPHI"
-scp -r ${RUNPHI_DIR}/target/runPHI_cell_configs/* ${RUNPHI_NODE_USER}@${RUNPHI_NODE_IP}:/usr/share/runPHI
+ssh ${RUNPHI_NODE_USER}@${RUNPHI_NODE_IP} "mkdir -p /usr/share/runPHI/include"
+scp ${RUNPHI_DIR}/target/target_configs/${TARGET}.toml ${RUNPHI_NODE_USER}@${RUNPHI_NODE_IP}:/usr/share/runPHI/platform-info.toml
+scp ${RUNPHI_DIR}/target/target_configs/${TARGET}_initial_state.toml ${RUNPHI_NODE_USER}@${RUNPHI_NODE_IP}:/usr/share/runPHI/state.toml
+
+# Define source and destination paths
+SOURCE_DIR="${RUNPHI_DIR}/target/runPHI_cell_configs"
+DEST_DIR="${RUNPHI_NODE_USER}@${RUNPHI_NODE_IP}:/usr/share/runPHI"
+
+# Step 1: Copy all contents of runPHI_cell_configs except qemu-cell.h and omnivisor-cell.h
+rsync -av --exclude 'include/qemu-cell.h' --exclude 'include/omnivisor-cell.h' "${SOURCE_DIR}/" "${DEST_DIR}"
+
+# Step 2: Determine the correct file to copy based on TARGET
+if [ "$TARGET" == "qemu" ]; then
+    FILE_TO_COPY="qemu-cell.h"
+else
+    FILE_TO_COPY="omnivisor-cell.h"
+fi
+
+# Step 3: Copy the selected file, renaming it to cell.h
+scp "${SOURCE_DIR}/include/${FILE_TO_COPY}" "${DEST_DIR}/include/cell.h"
+
+# scp -r ${RUNPHI_DIR}/target/runPHI_cell_configs/* ${RUNPHI_NODE_USER}@${RUNPHI_NODE_IP}:/usr/share/runPHI
 scp -r $(find ${RUNPHI_DIR}/target -maxdepth 1 -type f) ${RUNPHI_NODE_USER}@${RUNPHI_NODE_IP}:/root/target/
